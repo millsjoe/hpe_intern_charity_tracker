03ed17b37115524be655c91190e18fc1a02bf209
Joe Mills <joe.mills7157@gmail.com>
2019-01-31 11:30:32 +0000

    Added Licenses

--- hpe-charity-tracker/web/public/form_processing.php
+++ hpe-charity-tracker/web/public/form_processing.php
@@ -9,36 +9,20 @@
                 $x = 0;
                 $headers = [];
                 foreach($file as $line) {
-                    // echo $line . "<br><hr>";
                     $data = str_getcsv($line);
                     if($x==0){
-                        // foreach($data as $header){
-                        //     array_push($headers, $header);
-                        // }
                         $headers = $data;
                     }else {
                         echo "<p>Cause Name: ".$data[array_search('Cause Name', $headers)]."</p>";
                         echo "<p>Donation Date: ".$data[array_search('Donation Date', $headers)]."</p>";
-                        echo "<p>Donation Ammount: ".$data[array_search('Donation Ammount', $headers)]."</p>";
+                        echo "<p>Donation Ammount: ".$data[array_search('Donation Amount', $headers)]."</p>";
                         echo "<p>Transaction ID: ".$data[array_search('Transaction Id', $headers)]."</p>";
                     }
-                    // print_r(array_keys($data));
-                    // print_r($line);
-                    // print_r($data);
-
-                    echo "<hr>";
+                    echo "<hr>"; //TODO: Remove
                     $x++;
                 }
                 print_r($headers);
-                // print_r($data);
-                // for($i=1;$i<count($data);$i++){
-                //     // For every for in csv (excluding first row - headers)
-                //     print_r($data[$i]);
-                //     echo "<hr>";
-                // }
-                // // echo "hello";
             }
-            // echo "world";
         ?>
     </body>
 </html>
